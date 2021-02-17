9c276e651c0b98e08f03a68a50f9f6707cd75e04
FDLLA <fdo.dellano@gmail.com>
2021-02-17 11:30:16 -0600

    farma 4

--- js/farmacias.js
+++ js/farmacias.js
@@ -3,6 +3,30 @@ var urlFarmacias = "datos/farmacias.geojson";
 
 function addDatosFarmacias() {
 
+    var rivers = new L.GeoJSON.AJAX(
+        'https://raw.githubusercontent.com/nvkelso/natural-earth-vector/master/geojson/ne_10m_rivers_europe.geojson', {
+    
+            style: function (feature) {
+                return {
+                    color: "#00ffe1",
+                    weight: 6
+                }
+            },
+        }).addTo(map);
+
+    var carrilsBiciLineaStylePop = new L.GeoJSON.AJAX('datos/carrils-bici.geojson', {
+        style: function (feature) {
+            return {
+                color: "#d607f2",
+                weight: 6
+            }
+        },
+        onEachFeature: function (feature, layer) {
+            popupContentL = "<b>" + feature.properties.TOOLTIP + "</b>";
+            layer.bindPopup(popupContentL);
+        },
+    }).addTo(map);
+
          var puntosCluster = L.markerClusterGroup();
 
         layerFarmacias  = new L.GeoJSON.AJAX(urlFarmacias, {
