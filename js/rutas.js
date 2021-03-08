17ee4a47a5716130c3e882d22dbccc032276f6e5
FDLLA <fdo.dellano@gmail.com>
2021-03-07 22:34:26 -0600

    rotacion

--- js/rutas.js
+++ js/rutas.js
@@ -2,20 +2,20 @@
 function addRutas() {
 
     var url = 'datos/rutas.geojson';
-    map.addSource('rutas', { type: 'geojson', data: url});
+    map.addSource('rutas', { type: 'geojson', data: url });
 
-         map.addLayer({
-    'id': 'route',
-    'type': 'line',
-    'source': 'rutas',
-    'layout': {
-    'line-join': 'round',
-    'line-cap': 'round'
-    },
-    'paint': {
-    'line-color': '#28FF00',
-    'line-width': 3
-    }
+    map.addLayer({
+        'id': 'route',
+        'type': 'line',
+        'source': 'rutas',
+        'layout': {
+            'line-join': 'round',
+            'line-cap': 'round'
+        },
+        'paint': {
+            'line-color': '#28FF00',
+            'line-width': 3
+        }
     });
 
 } //fin funcion
@@ -23,10 +23,29 @@ function zoomToRutas(valores) {
 
     var coord = valores.split("/");
 
-    map.flyTo({
-            center: [coord[2], coord[1]],
-            zoom: coord[0]
-        });
+    console.info("coord",coord);
 
-    } //fin funcion
+    map.flyTo({
+        center: [coord[2], coord[1]],
+        zoom: coord[0]
+    });
+
+} //fin funcion
+var animacion;
+function rotarCamara(timestamp) {
+
+    //console.info("timestamp",timestamp);
+    rotacion =timestamp /100 ==360 ?0 : timestamp /100;
+    //console.info("rotacion",rotacion);
+    map.rotateTo(rotacion, { duration: 0 });
+
+    animacion = requestAnimationFrame(rotarCamara);
+}
+
+
+function finalRotarCamara() {
+
+    cancelAnimationFrame(animacion);
+}   
+
 
